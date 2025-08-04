38261d3f5a3f87be64314d2ebc7cb4c3d76be8a9
Naveenkumar12311 <naveen@spyd-tech.com>
2025-08-04 19:07:52 +0530

    port update

--- app3.py
+++ app3.py
@@ -3298,4 +3298,4 @@ if __name__ == '__main__':
     # ----------------------------------------
     # app.run(host='192.168.1.3',port=3000,debug=True)
     # app.run(debug=True)
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5001,debug=True)
